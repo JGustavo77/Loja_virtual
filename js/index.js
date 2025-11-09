7a718483600005957a33a7706951eb6db8de3534
lucaass-d3v <lucas.workps@gmail.com>
2025-11-09 17:07:29 -0300

    refactor: refatorado o arquivo index.js
    - Removidas linhas redundantes como 'produtos.style.transition = 1s all' em todos os ifs
    - Ifs seguem boas práticas da Object Calisthenics
    - Variável antes inacessível em trocarBtn() realocada e função renomeada para trocarBotao()
    - Renomeadas variáveis para maior clareza.

--- js/index.js
+++ js/index.js
@@ -1,130 +1,159 @@
-var widthTela = window.innerWidth;
+var larguraTela = window.innerWidth;
 const produtos = document.getElementById('produtos');
 var count = 0;
 
-if(produtos){
-    var widthBoxCard = document.querySelector('.box-card').clientWidth;
-    var widthVitrine = document.querySelector('.tela-produtos').clientWidth;
-    var clicks = 0;
+if (produtos) {
+
+    var larguraBoxCard = document.querySelector('.box-card').clientWidth;
+    var larguraVitrine = document.querySelector('.tela-produtos').clientWidth;
+    var quantidadeClicks = 0;
+
+    /*
+    Só é necessário fazer 'produtos.style.transition = '1s all'' uma vez. 
+    */
+    produtos.style.transition = '1s all';
+
+    /* 
+    Esse variável é usada quando um usuário mobile clica pra abrir o menu,
+    não estava sendo utilizada dentro da função menu(), realoquei ele para cá,
+    além disso, agr ela é 'var' assim, trocarBotao() tem acesso a ela, caso 'botaoMenu' estivesse dentro de
+    trocarBotao(), ele seria criada de novo e denovo.
+    */
+    var botaonMenu = document.getElementById('btnMenu');
+
     var vitrine;
     var padding;
-    var maxBoxItems = produtos.children.length;
+
+
+    var quantidadeMaximaItens = produtos.children.length;
     var startX = 0;
     var endX = 0;
 
-    if(widthVitrine <= 450){
-       vitrine = 1;
-       padding = 52;
-    }else if(widthVitrine > 450 && widthVitrine <= 1150){
+    if (larguraVitrine <= 450) {
+        vitrine = 1;
+        padding = 52;
+    } else if (larguraVitrine > 450 && larguraVitrine <= 1150) {
         vitrine = 2;
         padding = 50;
-    }else{
+    } else {
         vitrine = 3;
         padding = 50;
     }
 
-    produtos.addEventListener('touchstart', (e)=>{
+    produtos.addEventListener('touchstart', (e) => {
         startX = e.touches[0].clientX;
     });
 
-    produtos.addEventListener('touchend', (e)=>{
+    produtos.addEventListener('touchend', (e) => {
         endX = e.changedTouches[0].clientX;
         pegaDedo();
     });
 
-    function pegaDedo(){
-        const dedo = startX - endX;
+    function pegaDedo() {
+        const posicaoDedo = startX - endX;
 
-
-        if(Math.abs(dedo) > 100){
-            if(dedo > 0){
-                next();
-            }else{
-                prev();
-            }
+        if (Math.abs(posicaoDedo) > 100 && posicaoDedo > 0) {
+            next();
+            return;
         }
+
+        prev();
     }
 }
 
-function next(){
-    console.log(clicks);
-    if(clicks == 0){
-        clicks++;
+function next() {
+    console.log(quantidadeClicks);
 
-        produtos.style.transform = `translateX(${-clicks * (widthBoxCard + padding)}px)`;
-        produtos.style.transition = '1s all';
+    if (quantidadeClicks == 0) {
+        quantidadeClicks++;
 
-    }else if(clicks >= 1 && clicks < (maxBoxItems - vitrine)){
-        clicks++;
+        produtos.style.transform = `translateX(${-quantidadeClicks * (larguraBoxCard + padding)}px)`;
 
-        produtos.style.transform = `translateX(${-clicks * (widthBoxCard + padding)}px)`;
-        produtos.style.transition = '1s all';
-
-    }else{
-        clicks = 0;
-        produtos.style.transform = `translateX(0px)`;
+        return;
     }
+
+    if (quantidadeClicks >= 1 && quantidadeClicks < (quantidadeMaximaItens - vitrine)) {
+        quantidadeClicks++;
+
+        produtos.style.transform = `translateX(${-quantidadeClicks * (larguraBoxCard + padding)}px)`;
+
+        return;
+    }
+
+    quantidadeClicks = 0;
+    produtos.style.transform = `translateX(0px)`;
+
 }
 
-function prev(){
-    if(clicks == 0){
-        clicks = (maxBoxItems - vitrine);
+function prev() {
+    if (quantidadeClicks == 0) {
+        quantidadeClicks = (quantidadeMaximaItens - vitrine);
 
 
-        produtos.style.transform = `translateX(-${(clicks) * (widthBoxCard + padding)}px)`;
-        produtos.style.transition = '1s all';
+        produtos.style.transform = `translateX(-${(quantidadeClicks) * (larguraBoxCard + padding)}px)`;
 
-    }else if(clicks == maxBoxItems){
-        clicks = maxBoxItems;
-        
-        clicks--;
-
-        produtos.style.transform = `translateX(-${clicks * (widthBoxCard + padding)}px)`;
-        produtos.style.transition = '1s all';
-
-    }else if(clicks > 0 && clicks <= (maxBoxItems)){
-        
-        clicks--;
-
-        produtos.style.transform = `translateX(-${clicks * (widthBoxCard + padding)}px)`;
-        produtos.style.transition = '1s all';
-    } else{
-        clicks = 0;
-
-        produtos.style.transform = `translateX(${maxBoxItems *  widthBoxCard}px))`;
+        return;
     }
+
+    if (quantidadeClicks == quantidadeMaximaItens) {
+        quantidadeClicks = quantidadeMaximaItens;
+        quantidadeClicks--;
+
+        produtos.style.transform = `translateX(-${quantidadeClicks * (larguraBoxCard + padding)}px)`;
+
+        return;
+    }
+
+    if (quantidadeClicks > 0 && quantidadeClicks <= (quantidadeMaximaItens)) {
+        quantidadeClicks--;
+
+        produtos.style.transform = `translateX(-${quantidadeClicks * (larguraBoxCard + padding)}px)`;
+
+        return;
+    }
+
+    quantidadeClicks = 0;
+    produtos.style.transform = `translateX(${quantidadeMaximaItens * larguraBoxCard}px))`;
 }
 
-function menu(){
-    if(widthTela <= 600){
-        const btnMenu = document.getElementById('btnMenu');
+function menu() {
+
+    if (larguraTela <= 600) {
+
         const menu = document.getElementById('menu');
         const listaMenu = document.getElementById('listaMenu');
 
-        if(count == 0){
+        if (count == 0) {
             count = 1;
             menu.style.width = '100%';
             menu.style.padding = '50px 20px';
             menu.style.transition = '2s';
             listaMenu.style.display = 'flex';
-            trocarBtn(count);
-        }else if(count == 1){
+            trocarBotao(count);
+
+            return;
+        }
+
+        if (count == 1) {
             count = 0;
             menu.style.width = '0';
             menu.style.padding = '0px 0px';
             menu.style.transition = '2s';
-            trocarBtn(count);
-            // listaMenu.style.display = 'none';
+            trocarBotao(count);
+
+            return;
         }
-    }else{
+
         return;
     }
+
+    return;
 }
 
-function trocarBtn(count){
-    if(count == 1){
-        btnMenu.src = '/public/imgs/icons/x.png';
-    }else{
-        btnMenu.src = '/public/imgs/icons/arrow-right.png';
+function trocarBotao(count) {
+    if (count == 1) {
+        botaonMenu.src = '/public/imgs/icons/x.png';
+    } else {
+        botaonMenu.src = '/public/imgs/icons/arrow-right.png';
     }
 }
